1c73546fb2da721db33d1dbdedf06e29fc530fc5
André Formento <andreformento.sc@gmail.com>
2016-08-18 18:04:48 -0300

    first test

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # infinitest-continuous-testing
 Infinitest: Continuous Testing plugin for Eclipse and IntelliJ
+
+http://blog.caelum.com.br/executando-testes-de-unidade-continuamente-com-infinitest/
---
+++ src/test/java/com/formento/model/ProductTest.java
@@ -0,0 +1,14 @@
+package com.formento.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProductTest {
+
+    @Test
+    public void firstTest() {
+        assertTrue(true);
+    }
+
+}
